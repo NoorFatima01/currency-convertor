3ae8f559dfeb965a055a730bd558ce7fb37b3152
Noor Fatima <nfatima101204@gmail.com>
2024-06-24 23:51:34 +0500

    build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,16 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import exchangeRouter from './exchange';
+import path from 'path';
 
 const app = express();
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
