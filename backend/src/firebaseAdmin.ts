a716c911e186e3f3b896ab4bc2dd8d7ac7bd30e1
Noor Fatima <nfatima101204@gmail.com>
2024-06-26 01:49:24 +0500

    trying to fix serviceAccountKey not found during build

--- backend/src/firebaseAdmin.ts
+++ backend/src/firebaseAdmin.ts
@@ -1,6 +1,10 @@
 // src/firebaseAdmin.ts
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json";
+import * as fs from "fs";
+import path from "path";
+
+const serviceAccountPath = process.env.SERVICE_ACCOUNT_KEY_PATH || path.join(__dirname, 'serviceAccountKey.json');
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
